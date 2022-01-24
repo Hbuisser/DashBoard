7da73a6325991a4487e83723355117a7a79121b2
Henry Buisseret <henrybuisseret@MacBook-Pro-de-Henry.local>
2022-01-24 10:16:05 +0100

    userlist ok

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,26 @@ import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import "./app.css";
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import {
+	BrowserRouter as Router,
+	Navigate,
+	Routes,
+	Route,
+} from "react-router-dom";
 
 function App() {
 	return (
-		<div className="app">
+		<Router>
 			<Topbar />
 			<div className="container">
 				<Sidebar />
-				<Home/>
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="/users" element={<UserList />}></Route>
+				</Routes>
 			</div>
-		</div>
+		</Router>
 	);
 }
 
